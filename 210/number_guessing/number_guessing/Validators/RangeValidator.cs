1e1cc9d5f8246a2ef439a70bce966a97fc2a6811
vershinina618 <73005698+vershinina618@users.noreply.github.com>
2025-04-03 08:57:19 +0700

    develop

---
+++ 210/number_guessing/number_guessing/Validators/RangeValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number_guessing.Validators
+{
+    public class RangeValidator : IValidator<int>
+    {
+        private int _min;
+        private int _max;
+
+        public RangeValidator(int min, int max)
+        {
+            _min = min;
+            _max = max;
+        }
+
+        public int Min
+        {
+            get => _min;
+            set => _min = value;
+        }
+
+        public int Max
+        {
+            get => _max;
+            set => _max = value;
+        }
+
+        public ValidationResult Validate(int input)
+        {
+            if (input < _min || input > _max)
+                return new ValidationResult(false, $"Number must be between {_min}-{_max}");
+            return new ValidationResult(true, string.Empty);
+        }
+    }
+}
