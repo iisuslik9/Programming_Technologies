fb26be017e1a7f8dcffdc70f112f7840d2e47ab8
vershinina618 <73005698+vershinina618@users.noreply.github.com>
2025-04-03 08:41:35 +0700

    change range dynamicaly

---
+++ 210/number_guessing/number_guessing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using number_guessing.Validators;
+namespace number_guessing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rangeValidator = new RangeValidator(1, 100);
+            var game = new Game(new ConsoleOutput(), rangeValidator);
+
+            // Change range dynamically
+            game.UpdateRange(20, 80);
+            game.Play();
+        }
+    }
+}
