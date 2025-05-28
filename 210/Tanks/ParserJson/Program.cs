03580c6efab705c2b4bb17064d65d34ba96afaba
vershinina618 <73005698+vershinina618@users.noreply.github.com>
2025-05-29 04:34:31 +0700

    task2 part 2 template

---
+++ 210/Tanks/ParserJson/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+namespace ParserJson
+{
+    internal class Program
+    {
+        public class Deal
+        {
+            public int Sum { get; set; }
+            public string Id { get; set; }
+            public DateTime Date { get; set; }
+        }
+
+
+        IList<string> GetNumbersOfDeals(IEnumerable<Deal> deals)
+        {
+            //
+        }
+        record SumByMonth(DateTime Month, int Sum);
+
+        IList<SumByMonth> GetSumsByMonth(IEnumerable<Deal> deals)
+        {
+            //
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
