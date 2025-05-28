9434ca9c344143a8424c9db54e43bbdaa78d6406
vershinina618 <73005698+vershinina618@users.noreply.github.com>
2025-05-28 21:52:57 +0700

    dev template

---
+++ 210/Tanks/Tanks/Tank.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanks
+{   
+    /// <summary>
+    /// резервуар
+    /// </summary>
+    public class Tank
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Volume { get; set; }
+        public int MaxVolume { get; set; }
+        public int UnitId { get; set; }
+    }
+}
---
+++ 210/Tanks/Tanks/Unit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanks
+{
+    /// <summary>
+    /// установка
+    /// </summary>
+    public class Unit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int FactoryId { get; set; }
+    }
+}
