0c6ffce4e059a87ee4a86cb62c61b6054491d451
vershinina618 <73005698+vershinina618@users.noreply.github.com>
2025-05-29 01:54:17 +0700

    dev constructores

--- 210/Tanks/Tanks/Unit.cs
+++ 210/Tanks/Tanks/Unit.cs
@@ -15,5 +15,12 @@ namespace Tanks
         public string Name { get; set; }
         public string Description { get; set; }
         public int FactoryId { get; set; }
+        public Unit(int id, string name, string description, int factoryId)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            FactoryId = factoryId;
+        }
     }
 }
